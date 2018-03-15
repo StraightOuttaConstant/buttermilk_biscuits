71acb3c58158338c3ab9daa22292be36f32caf3b
JackMarx <seriousfools@gmail.com>
2018-03-15 18:21:34 -0500

    initial commit

---
+++ lib/buttermilk_biscuits.rb
@@ -0,0 +1,9 @@
+require "buttermilk_biscuits/version"
+
+module ButtermilkBiscuits
+  class BlowDryer
+    def dry
+      `say oooooooooooooooooooooooooooooooooooooooooooooooooooo`
+    end
+  end
+end
---
+++ lib/buttermilk_biscuits/version.rb
@@ -0,0 +1,3 @@
+module ButtermilkBiscuits
+  VERSION = "0.1.0"
+end
